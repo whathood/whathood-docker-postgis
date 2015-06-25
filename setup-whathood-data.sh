233c774db95be80a72603d1dfbe40d189be31f0e
Jim Smiley <jim.r.smiley@gmail.com>
2015-06-25 21:20:16 +0000

    second commit

---
+++ setup-whathood-data.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+db_user='whathood'
+#createuser -U $db_user --superuser whathood 2>&1 > /dev/null
+#createdb -U $db_user $db_user
+#psql -U $db_user -c "CREATE EXTENSION postis" $db_user
+#psql -U $db_user -c "CREATE EXTENSION pgrouting" $db_user
